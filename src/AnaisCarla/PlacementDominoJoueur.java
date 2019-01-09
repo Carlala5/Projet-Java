5b896f4de210c0bbc7997972878643f9c7dbc337
AnaisPe <an.perrier@sfr.fr>
2019-01-09 22:42:27 +0100

    update number 2.0

--- src/AnaisCarla/PlacementDominoJoueur.java
+++ src/AnaisCarla/PlacementDominoJoueur.java
@@ -9,25 +9,24 @@ import java.util.Scanner;
 
 
 
-/*
- * Cette fonction a pour but de demander au joueur des coordonées pour qu'il choisisse ou placer son domino
- */
 public class PlacementDominoJoueur {
 
+/*
+ * 
+ * Création du plateau
+ * 	
+ */
+	public enum Cell {O,C,Z}; // O for Empty, C for Castle, Z to test
 	
-	public enum Cell {E,C,D}; // E for Empty, C for Castle
-	
-	private static Cell [][] board;
-	
+	private static Cell [][] board;	
 	
 	public static void GameBoard() {
 		board = new Cell [5][5];
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) {
-				board [i][j] = Cell.E;
+				board [i][j] = Cell.O;
 			}
-		}
-		
+		}		
 	}
 	
 	public String toString() {
@@ -35,22 +34,28 @@ public class PlacementDominoJoueur {
 		for (Cell[]row : board) { // a for for each loop
 			for (Cell c : row) {
 				result += c;
+				result += " ";
 			}
 			result += "\n";
 	}
 		return result;
 	}
 
-	
 	public static void setCell(int row, int col, Cell value) { //particular method to change a particular cell
 		board[row][col] = value;		
 	}
-	    
-	public static void coordonnees_domino (int x1,int y1) {
+
+/*
+ * 
+ * Cette fonction a pour but de demander au joueur des coordonées pour qu'il choisisse ou placer son domino
+ * 
+ */
+ 
+	public static void coordonnees_domino () {   //(int x1,int y1)
 		List <Integer> liste = new ArrayList<>(Arrays.asList(0, 1, 2, 3 ,4));
 		int i = 0;
-//		int x1 = 0;
-//		int y1 = 0;
+		int x1 = 0;
+		int y1 = 0;
 		
 		Scanner scan = new Scanner(System.in);	
 		Scanner scan2 = new Scanner(System.in);
@@ -81,6 +86,10 @@ public class PlacementDominoJoueur {
 						System.out.println("Ce que vous avez entré ne correspond pas à une colonne du tableau.");
 						break;
 					}
+					
+					PlacementDominoJoueur b = new PlacementDominoJoueur();
+					b.setCell(x1, y1, Cell.Z);
+					System.out.println(b);
 					
 					Iterator<Integer> iterator2 = liste.iterator();
 					while (iterator2.hasNext()) {
@@ -116,29 +125,31 @@ public class PlacementDominoJoueur {
 			
 		}			
 	}
-			
-	
+
 		 public static void main(String[] args) {
-			 PlacementDominoJoueur b = new PlacementDominoJoueur();
+			PlacementDominoJoueur b = new PlacementDominoJoueur();
+			
+			
 			GameBoard();
 			
-			 
-			 
-			 
-			 /*We put a C in the middle, because at the beginning each castle of each player
-			  * is placed in the middle of the board
+			 /* We put a C at the left corner, because at the beginning each castle of each player
+			  * is placed at the left corner of the board
 			  */
-			 b.setCell(0, 0, Cell.C); // va de 0 à 8 (ligne,colonne)
-			 System.out.println(b);
-			 System.out.println("Veuillez entrer la ligne de la premiere case de votre domino (entre 0 et 4) ");
-			 Scanner scan = new Scanner(System.in);
-			 
-			 int x1 = scan.nextInt();
-			 System.out.println("Veuillez entrer la colonne de la premiere case de votre domino (entre 0 et 4) ");
-			 Scanner scan2 = new Scanner(System.in);
-			 int y1 = scan2.nextInt();
 			
-			 b.setCell(x1, y1, Cell.D);
-			 System.out.println(b);
+			 b.setCell(0, 0, Cell.C); // va de 0 à 8 (ligne,colonne)
+			 System.out.print(b);
+
+			//			 System.out.println("Veuillez entrer la ligne de la premiere case de votre domino (entre 0 et 4) ");
+//			 Scanner scan = new Scanner(System.in);
+//			 
+//			 int x1 = scan.nextInt();
+//			 System.out.println("Veuillez entrer la colonne de la premiere case de votre domino (entre 0 et 4) ");
+//			 Scanner scan2 = new Scanner(System.in);
+//			 int y1 = scan2.nextInt();
+//			
+//			 b.setCell(x1, y1, Cell.D);
+//			 System.out.println(b);
+		 
+			PlacementDominoJoueur.coordonnees_domino();
 		 }	
 }
