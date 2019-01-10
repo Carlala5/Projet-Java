3b066a2b0d384c18d5c6b1b7710c9e9c609bd561
Deruelle <carlany5@icloud.com>
2019-01-10 17:45:49 +0100

    lol

--- src/AnaisCarla/Jeu.java
+++ src/AnaisCarla/Jeu.java
@@ -5,17 +5,19 @@ package AnaisCarla;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Jeu {
 
 	Map <Integer, Dominos> ListeDomino = new HashMap<Integer, Dominos>();
+	ArrayList<Joueur> listeJoueur = new ArrayList<Joueur>();
 	Scanner reader = new Scanner(System.in);
-	Joueur joueur1 = new Joueur("","",0,1);
-	Joueur joueur2 = new Joueur("","",0,2);
-	Joueur joueur3 = new Joueur("","",0,3);
-	Joueur joueur4 = new Joueur("","",0,4);
+	Joueur joueur1;
+	Joueur joueur2;
+	Joueur joueur3;
+	Joueur joueur4;
 	
 public Jeu () {
 	menu();
@@ -29,7 +31,7 @@ public void menu() {
 	
 	switch (nbJoueurs) {
 	case 2 : deuxJoueurs();
-	premierTour(nbJoueurs);
+	//premierTour(nbJoueurs);
 	break;
 	case 3 : troisJoueurs();
 	break;
@@ -41,108 +43,184 @@ public void menu() {
 }
 
 }
-public void premierTour(int nbJoueurs) {
-	
-}
-public void deuxJoueurs () {
+
+
+public ArrayList<Joueur> deuxJoueurs () {
 	String entr = "";
 	
 	System.out.println("Joueur1, choisissez un nom d'utilisateur:");
 	entr = reader.next();
 	this.joueur1.setNom(entr);
+	this.joueur1.setScore(0);
+	this.joueur1.setNumeroJoueur(1);
 	System.out.println("Joueur1, choisissez maintenant votre couleur (rose, jaune, vert ou bleu ) :");
 	entr = reader.next();
 	this.joueur1.setCouleur(entr);
+	listeJoueur.add(joueur1);
+	
 	
 	System.out.println("Joueur2, choisissez un nom d'utilisateur:");
 	entr = reader.next();
 	this.joueur2.setNom(entr);
+	this.joueur2.setScore(0);
+	this.joueur2.setNumeroJoueur(2);
 	System.out.println("Joueur2, choisissez maintenant votre couleur (rose, jaune, vert ou bleu ) :");
 	entr = reader.next();
 	this.joueur2.setCouleur(entr);
+	listeJoueur.add(joueur2);
 // Verifier que la couleur n'a pas déjà été selectionnée
 	
-	
+	return listeJoueur;
 	
 	
 }
 
-public void troisJoueurs () {
+public ArrayList<Joueur>  troisJoueurs () {
 	String entr = "";
 	
 	System.out.println("Joueur1, choisissez un nom d'utilisateur:");
 	entr = reader.next();
 	this.joueur1.setNom(entr);
+	this.joueur1.setScore(0);
+	this.joueur1.setNumeroJoueur(1);
 	System.out.println("Joueur1, choisissez maintenant votre couleur (rose, jaune, vert ou bleu ) :");
 	entr = reader.next();
 	this.joueur1.setCouleur(entr);
+	listeJoueur.add(joueur1);
 	
 	System.out.println("Joueur2, choisissez un nom d'utilisateur:");
 	entr = reader.next();
 	this.joueur2.setNom(entr);
+	this.joueur2.setScore(0);
+	this.joueur2.setNumeroJoueur(2);
 	System.out.println("Joueur2, choisissez maintenant votre couleur (rose, jaune, vert ou bleu ) :");
 	entr = reader.next();
 	this.joueur2.setCouleur(entr);
+	listeJoueur.add(joueur2);
 	
 	System.out.println("Joueur3, choisissez un nom d'utilisateur:");
 	entr = reader.next();
 	this.joueur3.setNom(entr);
+	this.joueur3.setScore(0);
+	this.joueur3.setNumeroJoueur(3);
 	System.out.println("Joueur3, choisissez maintenant votre couleur (rose, jaune, vert ou bleu ) :");
 	entr = reader.next();
 	this.joueur3.setCouleur(entr);
+	listeJoueur.add(joueur3);
+	
+	return listeJoueur;
 }	
 	
-public void quatreJoueurs () {
+public ArrayList<Joueur>  quatreJoueurs () {
 
 	String entr = "";
 	
 	System.out.println("Joueur1, choisissez un nom d'utilisateur:");
 	entr = reader.next();
 	this.joueur1.setNom(entr);
+	this.joueur1.setScore(0);
+	this.joueur1.setNumeroJoueur(1);
 	System.out.println("Joueur1, choisissez maintenant votre couleur (rose, jaune, vert ou bleu ) :");
 	entr = reader.next();
 	this.joueur1.setCouleur(entr);
+	listeJoueur.add(joueur1);
 	
 	System.out.println("Joueur2, choisissez un nom d'utilisateur:");
 	entr = reader.next();
 	this.joueur2.setNom(entr);
+	this.joueur2.setScore(0);
+	this.joueur2.setNumeroJoueur(2);
 	System.out.println("Joueur2, choisissez maintenant votre couleur (rose, jaune, vert ou bleu ) :");
 	entr = reader.next();
 	this.joueur2.setCouleur(entr);
+	listeJoueur.add(joueur2);
 	
 	System.out.println("Joueur3, choisissez un nom d'utilisateur:");
 	entr = reader.next();
 	this.joueur3.setNom(entr);
+	this.joueur3.setScore(0);
+	this.joueur3.setNumeroJoueur(3);
 	System.out.println("Joueur3, choisissez maintenant votre couleur (rose, jaune, vert ou bleu ) :");
 	entr = reader.next();
 	this.joueur3.setCouleur(entr);
+	listeJoueur.add(joueur3);
 	
 
 	System.out.println("Joueur4, choisissez un nom d'utilisateur:");
 	entr = reader.next();
 	this.joueur4.setNom(entr);
+	this.joueur4.setScore(0);
+	this.joueur4.setNumeroJoueur(4);
 	System.out.println("Joueur4, choisissez maintenant votre couleur (rose, jaune, vert ou bleu ) :");
 	entr = reader.next();
 	this.joueur4.setCouleur(entr);
+	listeJoueur.add(joueur4);
+	
+	return listeJoueur;
 }		
 	
-public void tirageJoueur (int nbJoueurs) {
+public int tirageJoueur (int nbJoueurs) {
 	// appel aléatoire d'un joueur pour tirer au sort les rois:
 	
-	int i = (int) Math.random()*2;
+	int i = (int) Math.random()*nbJoueurs;
 	if (i == joueur1.getNumeroJoueur()){
 		System.out.println(joueur1.getNom() +" vous avez été sélectionné pour tirer les rois au sort");	
+		return i;
 	}
 	else if (i == joueur2.getNumeroJoueur()) {
 		System.out.println(joueur2.getNom() +" vous avez été sélectionné pour tirer les rois au sort");
+		return i;
 	}
 	else if (i == joueur3.getNumeroJoueur()) {
 		System.out.println(joueur3.getNom() +" vous avez été sélectionné pour tirer les rois au sort");
+		return i;
 	}
 	else if (i == joueur4.getNumeroJoueur()) {
 		System.out.println(joueur4.getNom() +" vous avez été sélectionné pour tirer les rois au sort");
+		return i;
 	}
+	return i;
 }
+
+//appuyer sur entrée pour générer un roi au hasard (pour la suite)
+
+
+
+public void premierTour(ArrayList<Joueur> listeJoueur) {
+	ArrayList<Joueur> listeJoueurBis = new ArrayList<Joueur>();
+	ArrayList<DominoCSVtoArrayList.Test> dominoCourant = new ArrayList<DominoCSVtoArrayList.Test>();
+	
+	
+	listeJoueurBis = listeJoueur;
+	int nbJoueurs;
+	int j;
+	//int numJoueur = tirageJoueur(nbJoueurs);
+	
+	do { 
+		nbJoueurs = listeJoueurBis.size(); 
+		j = (int)Math.random()*nbJoueurs;
+		String couleurRoi = listeJoueurBis.get(j).getCouleur();
+		System.out.println("Le roi de la couleur " +couleurRoi+ " doit jouer !");
+		
+		
+		
+		
+	}while(!listeJoueurBis.isEmpty());
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
 }
 
 
